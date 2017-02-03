21cf1eb9ade65df663bcae1fce880c20aa046ed2
Daichi Fukuoka <dc-fukuoka@sgi.com>
2017-02-03 09:25:49 +0900

    modified makefiles

--- makefile.impi.mk
+++ makefile.impi.mk
@@ -1,10 +1,17 @@
+fc       = ifort
+mpifc    = mpiifort
+fppflags = -fpp
+fflags   = -g -O3 -mavx
+ldflags  =
+libs     =
+
 all: create_input create_output diffuse
 create_input: create_input.F90
-	ifort -g -O3 -mavx create_input.F90 -o create_input
+	$(fc) $(fflags) $^ -o $@
 create_output: create_output.F90
-	ifort -g -O3 -mavx create_output.F90 -o create_output
+	$(fc) $(fflags) $^ -o $@
 diffuse: diffuse.F90
-	mpiifort -g -O3 -mavx diffuse.F90 -o diffuse
+	$(mpifc) $(fflags) $^ -o $@
 clean:
 	rm -f f create_input create_output diffuse *.mod *~ core.* data_in data_out
 	find . -type f -name "fort.*"|grep -v fort.11|xargs rm	
