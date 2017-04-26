031f957aa3e84edd79f673a48ca9ac772ebd3b43
Daichi Fukuoka <dc-fukuoka@sgi.com>
2017-04-26 10:59:20 +0900

    updated check.f90

--- check.f90
+++ check.f90
@@ -1,17 +1,20 @@
 program main
   implicit none
   integer::imax,jmax,kmax
+  integer::iter_max,tstep_max,freq_write
 
   namelist/param1/imax,jmax,kmax
-  integer::i,j,k
-  real(8),allocatable,dimension(:,:,:)::a,b
+  namelist/param3/iter_max,tstep_max,freq_write
+  integer::i,j,k,l
+  real(8),allocatable,dimension(:,:,:,:)::a,b
   real(8)::diff
   integer::len
   real(8),parameter::sigma=8.0d0
   real(8),parameter::tol=1.0d-12
 
   read(11,param1)
-  allocate(a(imax,jmax,kmax),b(imax,jmax,kmax))
+  read(11,param3)
+  allocate(a(imax,jmax,kmax,freq_write),b(imax,jmax,kmax,freq_write))
   open(45, file="data_out.orig", form="unformatted", access="stream")
   open(55, file="data_out",      form="unformatted", access="stream")
 
@@ -19,17 +22,20 @@ program main
   read(55) b
 
   write(6,*) "imax,jmax,kmax:",jmax,jmax,kmax
+  write(6,*) "freq_write:",freq_write
   write(6,*) "tol:",tol
-  !$omp parallel do private(i,j,k,diff)
-  do k=1,kmax
-     do j=1,jmax
-        do i=1,imax
-           diff = abs((b(i,j,k)-a(i,j,k))/a(i,j,k))
-           if (diff.ge.tol) then
-              write(6,*) "i,j,k:",i,j,k
-              write(6,*) "diff:", diff
-              stop
-           end if
+  !$omp parallel do private(i,j,k,l,diff)
+  do l=1,freq_write
+     do k=1,kmax
+        do j=1,jmax
+           do i=1,imax
+              diff = abs((b(i,j,k,l)-a(i,j,k,l))/a(i,j,k,l))
+              if (diff.ge.tol) then
+                 write(6,*) "i,j,k,l:",i,j,k,l
+                 write(6,*) "diff:", diff
+                 stop
+              end if
+           end do
         end do
      end do
   end do
